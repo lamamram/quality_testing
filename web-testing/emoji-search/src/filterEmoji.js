e485a8dc078af2e7b3ef98c1309ba1a9c7ace8e5
mlamamra <mlamamra@dawan.fr>
2023-01-16 14:24:49 +0100

    web-testing out

--- web-testing/emoji-search/src/filterEmoji.js
+++
@@ -1,9 +0,0 @@
-import emojiList from './emojiList.json';
-
-export default function filterEmoji(searchText, maxResults) {
-  return emojiList
-    .filter(emoji =>
-      emoji.title.toLowerCase().includes(searchText.toLowerCase())
-      || emoji.keywords.includes(searchText))
-    .slice(0, maxResults);
-}
--- web-testing/emoji-search/src/formats.test.js
+++
@@ -1,7 +0,0 @@
-import { longDateFormat } from './formats';
-
-test('test', () => {
-  var obj = { _longDateFormat: { 'NL': 'yyyyMMdd' } };
-  obj.longDateFormat = longDateFormat;
-  var result = obj.longDateFormat('nl');
-});
