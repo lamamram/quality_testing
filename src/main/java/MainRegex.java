aef2aae4808d0149a2fcf1ca8bc7d0259b16006f
mlamamra <mlamamra@dawan.fr>
2023-01-17 17:15:31 +0100

    exo regex

---
+++ src/main/java/MainRegex.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+import testing.regex.ZipCodeRegex;
+
+public class MainRegex {
+
+    public static void main(String[] args) {
+        String pattern = "2[0-9AB][0-9]{3}|[013456789][0-9]{4}";
+        ZipCodeRegex zr = new ZipCodeRegex(pattern);
+        List<String> matches= zr.findAll("mon zipcode est 44245 ou 2A345");
+        for (String m : matches) {
+            System.out.println(m);
+        }
+    }
+}
---
+++ src/main/java/testing/regex/ZipCodeRegex.java
@@ -0,0 +1,33 @@
+package testing.regex;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ZipCodeRegex {
+    private Pattern pattern;
+    
+    public ZipCodeRegex(String patternString) {
+        this.pattern = Pattern.compile(patternString);
+    }
+    
+    public String find(String target) {
+        String ret = null;
+        Matcher matcher = pattern.matcher(target);
+        if (matcher.find()) {
+            ret = matcher.group();
+        }
+        return ret;
+    }
+    
+    public List<String> findAll(String target) {
+        Matcher matcher = pattern.matcher(target);
+        List<String>lst = new ArrayList<>(); 
+        while(matcher.find()) {
+            lst.add(matcher.group());
+        }
+        return lst;
+    }
+}
