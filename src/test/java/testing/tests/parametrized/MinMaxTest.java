1597e71d3a95d087d8bb60b7a551e22c3d510786
mlamamra <mlamamra@dawan.fr>
2023-01-19 14:01:02 +0100

    reports

--- src/test/java/testing/tests/parametrized/MinMaxTest.java
+++ src/test/java/testing/tests/parametrized/MinMaxTest.java
@@ -1,12 +1,15 @@
 package testing.tests.parametrized;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static java.util.stream.Collectors.toList;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import testing.parametrized.MinMax;
@@ -19,7 +22,8 @@ class MinMaxTest {
     // la méthode génératrice
     @ParameterizedTest
     @MethodSource("generator")
-    void testMinMax(int[] nums, int expectedMin, int expectedMax) {
+    //@ValueSource({{1, 3, 4}, 1, 4})
+    void testMinMaxGenerator(int[] nums, int expectedMin, int expectedMax) {
         mm.find(nums);
         assertEquals(expectedMin, mm.getMin());
         assertEquals(expectedMax, mm.getMax());
@@ -32,6 +36,24 @@ class MinMaxTest {
         Arguments ac1 = Arguments.of(new int[]{2, 100, 33, -44}, -44, 100 );
         Arguments ac2 = Arguments.of(new int[]{7, 22, -7, 88}, -7, 88 );
         return Stream.of(ac1, ac2);
+    }
+    
+    //@ParameterizedTest
+    // fichiers dans src/test/resources
+    // src/test/resources doit être dans le classpath
+    // voir pom.xml -> build -> resources
+    @CsvFileSource(resources = "/nums.csv")
+    void testMinMaxCsv(int expectedMin, int expectedMax, String... nums) {
+        int[] realNums = new int [nums.length];
+        for (int i=0; i<nums.length; i++) {
+            realNums[i] = Integer.parseInt(nums[i]);
+        }
+        for (int i : realNums) {
+            System.out.println(i);
+        }
+        mm.find(realNums);
+        assertEquals(Integer.valueOf(expectedMin), mm.getMin());
+        assertEquals(Integer.valueOf(expectedMax), mm.getMax());
     }
 
 }
