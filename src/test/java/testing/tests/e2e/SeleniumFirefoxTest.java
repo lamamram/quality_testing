cab14fabdd70fee60807299889d4d79371ad6d64
mlamamra <mlamamra@dawan.fr>
2023-01-20 12:31:06 +0100

    test selenium on two pages

--- src/test/java/testing/tests/e2e/SeleniumFirefoxTest.java
+++ src/test/java/testing/tests/e2e/SeleniumFirefoxTest.java
@@ -1,27 +1,57 @@
 package testing.tests.e2e;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.*;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 
 class SeleniumFirefoxTest {
 	
 	private WebDriver driver;
+
+	@BeforeEach
+	public void connect() {
+	    driver = new FirefoxDriver();
+	    // timeout max de chargement de page
+	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	@AfterEach
+	public void close() {
+	    driver.close();
+	}
 	
 	@Test
 	@Tag("E2E")
-	void test() {
+	void testHTPriceForSeleniumDawan() {
 	    //System.setProperty("webdriver.gecko.driver", "Chemin vers le driver, préférer le PATH");
-		this.driver = new FirefoxDriver();
-        this.driver.get("https://dawan.fr");
-        assertThat(this.driver.getTitle()).contains("Dawan");
-		this.driver.close();
+	    this.driver.get("https://dawan.fr");
+        // sélection d'un élément DOM comme en javascript
+        WebElement searchInput = driver.findElement(By.id("motsclefs"));
+        // entrée clavier
+        searchInput.sendKeys("selenium");
+        //searchInput.sendKeys(Keys.RETURN);
+        WebElement submitSearch = driver.findElement(By.xpath("//button[@class='pre-menu-search-submit border-form']"));
+        submitSearch.click();
+        // chercher un Matcher Hamcrest ou...
+        List<WebElement> priceContainers = driver.findElements(By.className("result-training-price"));
+        List<String> prices = priceContainers.stream().
+                map(item -> item.getText()).collect(Collectors.toList());
+        for (String p : prices) {
+            assertThat(p).contains("1790 €");
+        }
 	}
 
 }
